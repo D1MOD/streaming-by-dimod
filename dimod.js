777e47c7d3d8f74ab28fa3a4491f0989597dbfae
D1MOD <91766153+D1MOD@users.noreply.github.com>
2021-10-02 04:25:23 +0300

    Create dimod.js

---
+++ dimod.js
@@ -0,0 +1,28 @@
+
+const Discord = require("discord.js");
+const dimod = new Discord.Client();
+//////// playing or dnd  streaming ////////
+dimod.on("ready", () => {
+  console.log(
+    `Online In Servers`
+  );
+  let statuses = [
+  
+  `D1MOD.`,
+ 
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    dimod.user.setActivity(STREAMING, {      
+    type: "STREAMING", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+///////////////////////
+dimod.on("ready", () => {
+    var join = dimod.channels.get(""); /// id voice
+    if (join) join.join();
+  });
+////////////////   TOKEN LERA DABNE 
+dimod.login("");
